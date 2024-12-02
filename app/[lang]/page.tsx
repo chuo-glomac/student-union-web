5496a75ee062572873ba06ffac8c3c28b5f2c7c4
kuisin <kaisei0807s@gmail.com>
2024-12-02 20:55:44 +0900

    update top page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,14 +1,20 @@
+import { LanguageSwitcher } from "@/components/changeLanguageButton";
+import { useRouter } from "next/navigation";
+
 export default async function LangPage({
   params,
 }: {
-  params: Promise<{ lang: string }>
+  params: Promise<{ lang: string }>;
 }) {
+  const router = useRouter();
   const { lang } = await params;
 
   return (
     <div>
       <h1>Welcome to the Top Page!</h1>
       <p>Language: {lang}</p>
+      <LanguageSwitcher />
+      <a onClick={() => router.push(`/${lang}/login`)}>To Home</a>
     </div>
   );
 }
